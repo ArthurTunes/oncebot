be4314eec0a7a4ef4a2126456fa02ffe6d75cbb6
rtwo <57819536+ArthurTunes@users.noreply.github.com>
2022-10-27 00:12:32 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,15 +3,9 @@ if [ "$EUID" -ne 0 ]
         then echo "Please run as root."
         exit 1
 fi
-
-apt update
-apt install curl git -y
-
-git clone https://github.com/ArthurTunes/oncebot
-chmod 755 /oncebot/once.sh
-cp /oncebot/once.sh /usr/bin/oncebot
+chmod 755 once.sh
+cp once.sh /usr/bin
 rm -rf oncebot
-
 echo "Hope u enjoy :)"
 echo
 echo "Signed by rtwo"
