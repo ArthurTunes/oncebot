77c892d507ffa9675523da21f8f75f05386c9d8e
rtwo <57819536+ArthurTunes@users.noreply.github.com>
2022-10-27 18:08:14 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,19 @@ fi
 chmod 755 oncebot/once.sh
 cp oncebot/once.sh /usr/bin/once
 rm -rf oncebot
+sudo apt-get install -y -qq pkg-config libssl-dev libxcb1-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev
+mkdir .music
+exo-open --launch TerminalEmulator
+cd .music
+youtube-dl --extract-audio --audio-format mp3 https://youtu.be/HQXBAGVSOyY
+youtube-dl --extract-audio --audio-format mp3 https://youtu.be/gzOdfzuFJ3E
+youtube-dl --extract-audio --audio-format mp3 https://youtu.be/LKHm2N-knDQ
+youtube-dl --extract-audio --audio-format mp3 https://youtu.be/f5_wn8mexmM
+cd ..
+exit
+
 echo "Hope u enjoy :)"
 echo
 echo "Signed by rtwo"
+echo
 echo "aka NayeonLindinha '>.<"
